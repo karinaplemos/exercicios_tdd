e8fa0219d0fd970300bd1aadfa6d609b3dbe8860
Karina Pereira de Lemos <karinalemos@id.uff.br>
2022-04-18 20:44:43 -0300

    Caso de teste que vai passar para apenas um boleto

--- test/processadorBoletos/processadorBoletosTest.java
+++ test/processadorBoletos/processadorBoletosTest.java
@@ -2,10 +2,16 @@ package processadorBoletos;
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.Date;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import boleto.Boleto;
+import fatura.Fatura;
 
 class processadorBoletosTest {
 	
+	@DisplayName("Teste para um boleto")
 	@Test
 	public void testPagamento() {
 		Fatura fatura = new Fatura("Karina Lemos", 122.00, new Date());
